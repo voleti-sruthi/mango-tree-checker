0a060cea29ce3ab15bba901d60bd4dc1dfcecb77
voleti-sruthi <sruthi.lakshmi@m2pfintech.com>
2023-03-24 22:55:34 +0530

    Add. included code to check if the tree is a mango tree

--- src/MangoTreeChecker.java
+++ src/MangoTreeChecker.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class MangoTreeChecker {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of Rows: ");
+        int noOfRows = scanner.nextInt();
+        System.out.println("Enter the number of columns: ");
+        int noOfColumns = scanner.nextInt();
+        System.out.println("Enter tree number to be checked");
+        int treeNoToCheck = scanner.nextInt();
+        if(treeNoToCheck > noOfRows*noOfColumns || noOfRows <= 0 || noOfColumns <= 0 || treeNoToCheck <= 0){
+            System.out.println("no");
+        }
+        else if(treeNoToCheck <= noOfColumns || treeNoToCheck % noOfColumns == 1 || treeNoToCheck % noOfColumns == 0){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
     }
 }
